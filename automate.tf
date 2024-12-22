4a14b4193d7869a16cf1bfa1c7cd082d82f0fce1
KrushnakantPatel <121893713+KrushnakantPatel@users.noreply.github.com>
2024-12-22 12:35:52 -0500

    Update automate.tf

--- automate.tf
+++ automate.tf
@@ -3,16 +3,19 @@ This is the demo if we need more than one subnets(Public and Private) then we ca
 
 # Public Subnets (Automated for multiple subnets)
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidrs)
-  vpc_id                  = aws_vpc.main.id
+  count                   = length(var.public_subnet_cidrs)  ##Specifies how many instance of this resource Terraform should create
+  #The above line dynamically calculates the number of public subnets by checking how many CIDR blocks are provided in the public_subnet_cidr variable
+  vpc_id                  = aws_vpc.main.id   ##Associate to the main VPC 
   cidr_block              = var.public_subnet_cidrs[count.index]
-  map_public_ip_on_launch = true
+  #[count.index] will perform as the current iteration index (0,1,2,...) of the loop 
+  map_public_ip_on_launch = true   #To ensure that any resource launched in the above subnet automatically gets the IP address 
 
   tags = {
-    Name = "PublicSubnet-${count.index + 1}"
+    Name = "PublicSubnet-${count.index + 1}"  #Name the first subnet as PublicSubnet-1 and second one as PublicSubnet-2
   }
 }
 
+##Similarly for the private Subnets 
 # Private Subnets (Automated for multiple subnets)
 resource "aws_subnet" "private" {
   count      = length(var.private_subnet_cidrs)
