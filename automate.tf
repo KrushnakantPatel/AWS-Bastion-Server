af8d61637325c38f0e744e6810dc9f6b00440309
KrushnakantPatel <121893713+KrushnakantPatel@users.noreply.github.com>
2024-12-23 19:25:30 -0500

    Update automate.tf-

--- automate.tf
+++ automate.tf
@@ -1,4 +1,4 @@
-This is the demo if we need more than one subnets(Public and Private) then we can change the script accordingly
+#This is the demo if we need more than one subnets(Public and Private) then we can change the script accordingly
 
 #Keywords used
 # Terraform looping with count: Uses count to repeat resource creation
@@ -8,10 +8,10 @@ This is the demo if we need more than one subnets(Public and Private) then we ca
 
 # Public Subnets (Automated for multiple subnets)
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidrs)  ##Specifies how many instance of this resource Terraform should create
+  count                   = length(var.public_subnet_cidr)  ##Specifies how many instance of this resource Terraform should create
   #The above line dynamically calculates the number of public subnets by checking how many CIDR blocks are provided in the public_subnet_cidr variable
   vpc_id                  = aws_vpc.main.id   ##Associate to the main VPC 
-  cidr_block              = var.public_subnet_cidrs[count.index]
+  cidr_block              = var.public_subnet_cidr[count.index]
   #[count.index] will perform as the current iteration index (0,1,2,...) of the loop 
   map_public_ip_on_launch = true   #To ensure that any resource launched in the above subnet automatically gets the IP address 
 
@@ -23,9 +23,9 @@ resource "aws_subnet" "public" {
 ##Similarly for the private Subnets 
 # Private Subnets (Automated for multiple subnets)
 resource "aws_subnet" "private" {
-  count      = length(var.private_subnet_cidrs)
+  count      = length(var.private_subnet-cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidrs[count.index]
+  cidr_block = var.private_subnet-cidr[count.index]
 
   tags = {
     Name = "PrivateSubnet-${count.index + 1}"
